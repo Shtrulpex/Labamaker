2253bf0dac4cfa3099664d7f0fd572f4ade615f8
CKAPJIATuHA <fedorov.va@phystech.edu>
2022-04-28 00:25:10 +0300

    class Data: NEW!     1) 'form_tables' and 'form_texts' methods     2) 'getters' class Table: NEW!     1) inheritance from pandas.DataFrame     2) 'convert' method for Serias of Table class Parameter NEW!     3) 'convert' method for parameter class Kinds: NEW!     1) added enum for TextOption initialization class Text:     1) Upgraded initialize method with templates class Text_Option:     1) Added parameter: "underline" /////////////// 1) Added examples of source filse:     -param_1.json     -text_1.json     -measures_1.xlsx 2) Upgraded units.json:     Added standard units

--- engine/data/data.py
+++ engine/data/data.py
@@ -1,3 +1,4 @@
+import enum
 import pandas as pd
 from pandas import DataFrame, read_csv
 import os
@@ -24,30 +25,86 @@ class MeasUnit:
     def get_multiplier(self):
         return self.kind.get_multiplier()
 
-    def transform(self, new_kind: Kind):
-        self.kind = new_kind
-
 
 class TextOption:
-    def __init__(self, font='arial', size=12, bold=False,
-                 color='black', italics=False, frame=False):
+    def __init__(self, font: str = 'arial',
+                 size: int = 12,
+                 bold: bool = False,
+                 color: str = 'black',
+                 italics: bool = False,
+                 frame: bool = False,
+                 underline: bool = False) -> object:
         self.font = font
         self.size = size
         self.bold = bold
         self.color = color
         self.italics = italics
         self.frame = frame
+        self.underline = underline
+
+
+class Kinds(enum.Enum):
+    text = 'text'
+    formula = 'formula'
+    list = 'list'
+    title = 'title'
 
 
 class Text:
-    def __init__(self, text, kind, text_option):
+    def __init__(self, text_option: dict, kind: str, text: str):
         self.text = text
-        self.kind = kind  # text/formula/list
-        self.text_option = text_option
+        self.kind = kind  # text/formula/list/title
+        if kind == Kinds.text:
+            self.text_option = TextOption(font='arial',
+                                          size=12,
+                                          bold=False,
+                                          color='black',
+                                          italics=False,
+                                          frame=False,
+                                          underline=False)
+        elif kind == Kinds.formula:
+            self.text_option = TextOption(font='arial',
+                                          size=12,
+                                          bold=False,
+                                          color='black',
+                                          italics=False,
+                                          frame=True,
+                                          underline=False)
+        elif kind == Kinds.title:
+            self.text_option = TextOption(font='arial',
+                                          size=16,
+                                          bold=True,
+                                          color='black',
+                                          italics=False,
+                                          frame=False,
+                                          underline=True)
+        elif kind == Kinds.list:
+            self.text_option = TextOption(font='arial',
+                                          size=12,
+                                          bold=False,
+                                          color='black',
+                                          italics=True,
+                                          frame=False,
+                                          underline=True)
+        else:
+            font = text_option['font'],
+            size = text_option['size'],
+            bold = text_option['bold'],
+            color = text_option['color'],
+            italics = text_option['italics'],
+            frame = text_option['frame'],
+            underline = text_option['underline']
+            self.text_option = TextOption(font=font,
+                                          size=size,
+                                          bold=bold,
+                                          color=color,
+                                          italics=italics,
+                                          frame=frame,
+                                          underline=underline)
 
 
 class ParamOptions:
-    def __init__(self, value_option, unit_option, name_option):
+    def __init__(self, value_option: TextOption, unit_option: TextOption, name_option: TextOption):
         self.value_option = value_option
         self.unit_option = unit_option
         self.name_option = name_option
@@ -65,7 +122,8 @@ class Parameter:
         self.unit = unit
         self.options = options
 
-    def transform(self, new_unit: MeasUnit):
+    # Transform from one unit to other (P -> N/m^2) (Па ->  Н/м^2) OR change the prefix
+    def convert(self, new_unit: MeasUnit):
         if new_unit.get_category() != self.unit.get_category():
             raise RuntimeError('Incorrect unit to transform')
         self.unit = new_unit
@@ -74,55 +132,85 @@ class Parameter:
         pass
 
 
-# form with 2 ways (from table and from pandas)
 class Table(DataFrame):
     def __init__(self, name, *args):
         self.name = name
         super().__init__(*args)
 
-    def transform(self, func: Parameter.transform, axis: Axis = 0, *args, **kwargs) -> DataFrame:
-        pass
-
-
-data = [{'a': 1, 'b': 2, 'c':3},
-        {'a':10, 'b': 20, 'c': 30}]
-
-t = Table(data, index=['val', 'too'])
-t2 = Table(read_csv('123.csv'))
-print(t2)
-print(type(t2))
+    def convert(self, column, new_unit: MeasUnit):
+        for param in self[column]:
+            param.convert(new_unit)
 
 
 class Data:
     def __init__(self, folder):
         self.folder = folder
-        self.table = self.form_tables()
-        self.text = self.form_texts()
+        self.tables = self.__form_tables()  # dict of tables: {name: table_obj}
+        self.texts = self.__form_texts()  # list of texts (titles, lists, simple texts or formulas)
 
-    def get_table(self, *name):
-        if name:
-            return self.table[name]
-        return self.table
-
-    def get_text(self, *name):
-        if name:
-            return self.text[name]
-        return self.text
-
-    def get_description(self):
-        return self.folder + '\\description\\description.pdf'
-
-    def form_tables(self) -> dict:
+    def __form_tables(self) -> dict:
         path = self.folder + '\\tables'
         files = os.listdir(path=path)
         tables = dict()
         for file in files:
             name = file.split('\\')[-1].split('.')[0]
-            table = Table(name, pd.read_csv(file))
+            table = Table(name, pd.read_csv(file, index_col=0))
             tables[name] = table
         return tables
 
-    def form_texts(self) -> dict:
-        desc_path = self.folder + '\\text\\description.pdf'
+    def __form_texts(self) -> list:
+        path = self.folder + '\\texts'
+        files = os.listdir(path=path)
+        texts = []
+        for file in files:
+            with open(file, 'r', encoding='utf8') as f:
+                data = json.loads(f)
+                texts_ = data['texts']
+                for txt in texts_.keys():
+                    text = Text(texts_[txt]['options'],
+                                texts_[txt]['kind'],
+                                texts_[txt]['text'])
+                    texts.append(text)
+        return texts
 
+    def get_table(self, *name):
+        if name:
+            return self.tables[name]
+        return self.tables
+
+    def get_text(self, *name):
+        if name:
+            return self.texts[name]
+        return self.texts
+
+    def get_description(self):
+        return self.folder + '\\description\\description.pdf'
+
+
+class DataController:
+    def __init__(self, folder):
+        self.folder = folder
+        self.source = None
+        self.material = None
+        self.result = None
+        self.__generate_data()
+
+    def __generate_data(self):
+        source_folder = self.folder + '\\source'
+        self.source = Data(source_folder)
+
+        material_folder = self.folder + '\\material'
+        self.material = Data(material_folder)
+
+        result_folder = self.folder + '\\result'
+        self.result = Data(result_folder)
+
+    def get_source(self):
+        return self.source
+
+    def get_materials(self):
+        return self.material
+
+    def get_result(self):
+        return self.result
 
