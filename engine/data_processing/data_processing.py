820266745154b01441b3db21d2543175c4165490
Ivan <issibirtsev@gmail.com>
2022-04-28 09:13:53 +0300

    reworkMLS

--- engine/data_processing/data_processing.py
+++ engine/data_processing/data_processing.py
@@ -1,26 +1,75 @@
 import numpy as np
+from enum import Enum, auto
+
+
+class Data(Enum):
+    X = auto()
+    Y = auto()
+    K = auto()
+    B = auto()
+    DK = auto()
+    DB = auto()
 
 
 class Method:
-    def __init__(self, result, arguments):
+    def __init__(self, result, arguments, sequence):
         self.result = result
         self.args = arguments
+        self.sequence = sequence
+
+    def calc(self):
+        pass
+
+
+class CalcData:
+    @staticmethod
+    def do(d):
+        return {Data.X: [1, 2, 3, 4, 5]}
+    # парсит на x и y, но хз как
+
+
+class CalcK(CalcData):
+    @staticmethod
+    def do(d):
+        a = np.vstack([d[Data.X], np.ones(len(d.Data.X))]).T
+        k, b = np.linalg.lstsq(a, d[Data.Y], rcond=None)[0]
+        return d + {Data.K: k}
+
+
+class CalcB(CalcData):
+    @staticmethod
+    def do(d):
+        a = np.vstack([d[Data.X], np.ones(len(d.Data.X))]).T
+        k, b = np.linalg.lstsq(a, d[Data.Y], rcond=None)[0]
+        return d + {Data.B: b}
+
+
+class CalcDK(CalcData):
+    @staticmethod
+    def do(d):
+        x_2 = np.array(d[Data.X]).mean() ** 2
+        y_2 = np.array(d[Data.Y]).mean() ** 2
+        x2_ = (np.array(d[Data.X]) ** 2).mean()
+        y2_ = (np.array(d[Data.Y]) ** 2).mean()
+        dk = (y2_ - y_2) / (x2_ - x_2) - np.array(d[Data.B]) ** 2
+        dk **= 0.5
+        dk /= len(d[Data.X]) ** 0.5
+        return d + {Data.DK: dk}
+
+
+class CalcDB(CalcData):
+    @staticmethod
+    def do(d):
+        x_2 = np.array(d[Data.X]).mean() ** 2
+        x2_ = (np.array(d[Data.X]) ** 2).mean()
+        return d + {Data.DB: d.Data.DK * (x2_ - x_2) ** 0.5}
 
 
 class MLS(Method):
-    def do(self):
-        x = self.args['x']
-        y = self.args['y']
-        a = np.vstack([x, np.ones(len(x))]).T
-        k, b = np.linalg.lstsq(a, y, rcond=None)[0]
-        x_2 = x.mean()**2
-        y_2 = y.mean()**2
-        x2_ = (x**2).mean()
-        y2_ = (y**2).mean()
-        dk = (y2_-y_2)/(x2_-x_2) - b**2
-        dk **= 0.5
-        dk /= len(x)**0.5
-        db = dk * (x2_ - x_2)**0.5
-        self.result = {'k': k, 'b': b, 'dk': dk, 'db': db}
-        return self.result
+    def __init__(self, arguments):
+        super().__init__(CalcData.do(arguments), arguments, [CalcData, CalcK, CalcB, CalcDK, CalcDB])
 
+    def calc(self):
+        for i in self.sequence:
+            self.result = i.do(self.result)
+        return self.result
