484bcefe111659117fa0d05e0c1d244db9911ffe
Vsevolod Fedorov <v3531946@hotmail.cpm>
2022-05-25 02:48:29 +0300

    reworked Prefix          Kind -> (BaseKind && DerivedKind)          MeasUnit reworked "param.json" && "units.json" files, like already after parser

--- engine/data/data.py
+++ engine/data/data.py
@@ -5,14 +5,15 @@ import shutil
 import json
 
 
+# the entity specifying the multiplier and the simple prefix string
 class Prefix:
     # load list of prefixes dictionaries from file. Example:
     # "micro": [-6, "мк", "mk"] etc.
     with open('units.json', 'r', encoding='utf8') as f:
         data = json.load(f)
         __prefixes = data['prefix']  # key - full name of prefix
-        __en_prefixes = dict()       # key - short EN name of prefix
-        __ru_prefixes = dict()       # key - short RU name of prefix
+        __en_prefixes = dict()  # key - short EN name of prefix
+        __ru_prefixes = dict()  # key - short RU name of prefix
         for current_prefix in data['prefix'].keys():
             __en_prefixes[data['prefix'][current_prefix][2]] = current_prefix
             __ru_prefixes[data['prefix'][current_prefix][1]] = current_prefix
@@ -29,9 +30,9 @@ class Prefix:
         else:
             raise RuntimeError(f'incorrect prefix: {prefix}')
         self.__prefix = Prefix.__prefixes[full_name_of_prefix]
-        self.multiplier = Prefix.__prefixes[full_name_of_prefix][0]
+        self.__multiplier = Prefix.__prefixes[full_name_of_prefix][0]
 
-    def get_prefix(self, language='en'):
+    def get_prefix_string(self, language='en'):
         if language == 'en':
             return self.__prefix[2]
         elif language == 'ru':
@@ -39,18 +40,146 @@ class Prefix:
         raise RuntimeError(f"incorrect language: {language}\n"
                            f"(accessible: 'en' or 'ru', not {language})")
 
+    def get_multiplier(self):
+        return self.__multiplier
+
 
 class Kind:
-    def __init__(self, name: str, prefix: str):
-        self.name = name
-        self.prefix = Prefix(prefix)
-        self.multiplier = 10 ** self.prefix.multiplier
+    with open('units.json', 'r', encoding='utf8') as f:
+        _derived_units = json.load(f)['derived_units']
+
+    with open('units.json', 'r', encoding='utf8') as f:
+        _base_units = json.load(f)['base_units']
+
+    def __init__(self):
+        self.__multiplier = None
 
     def get_multiplier(self):
-        return self.multiplier
+        return self.__multiplier
 
-    def get_unit(self):
-        return f'{self.prefix.get_prefix()}{self.name}'
+    def get_unit_string(self):
+        pass
+
+
+# the kind of derived_unit
+# with particular prefixes
+# Examples:
+#   (m) || (mm) || (s)
+# P.S. you can see list of base_units in "units.json\base_units"
+class BaseKind(Kind):
+    def __init__(self, prefix: str, base_unit: str):
+        super().__init__()
+        self.__prefix = Prefix(prefix)
+        self.__unit = BaseKind._base_units[base_unit][1]
+        self.__multiplier = self.__prefix.get_multiplier()
+
+    def get_unit_string(self):
+        return f'{self.__prefix.get_prefix_string()}' \
+               f'{self.__unit}'
+
+    def set_prefix(self, prefix: str):
+        pass
+
+    def to_si(self):
+        pass
+
+
+# the kind of derived_unit
+# with particular prefixes
+# Examples:
+#   (m)/(s^2) || (mm)/(s^2),
+#   (J) || (kg*m^2)/(s^2) <- physical work
+class DerivedKind(Kind):
+    class Fraction(enum.Enum):
+        numerator = 'numerator'
+        denominator = 'denominator'
+        base = 'base'
+        derived = 'derived'
+
+    s1 = Fraction.numerator.value
+    s2 = Fraction.denominator.value
+
+    @classmethod
+    def init_from_file(cls, data: dict):
+        numerator = DerivedKind.s1
+        denominator = DerivedKind.s2
+        base = DerivedKind.Fraction.base.value
+        derived = DerivedKind.Fraction.derived.value
+
+        numer = []
+        denomin = []
+
+        if DerivedKind.s1 not in data.keys():
+            raise RuntimeError(f"can't find keys: {DerivedKind.s1}")
+        if DerivedKind.s2 not in data.keys():
+            raise RuntimeError(f"can't find keys: {DerivedKind.s2}")
+        for base_kind in data[numerator][base]:
+            numer.append(BaseKind(
+                base_kind[0],
+                base_kind[1]
+            ))
+        for derived in data[numerator][derived]:
+            numer.append(cls.__init_single(**derived))
+        for base_kind in data[denominator][base]:
+            denomin.append(BaseKind(
+                base_kind[0],
+                base_kind[1]
+            ))
+        for derived in data[denominator][derived]:
+            denominator.append(cls.__init_single(**derived))
+        d = {'numerator': numer,
+             'denominator': denomin}
+        return cls(**d)
+
+    # init_single_derived_unit_from_file
+    @classmethod
+    def __init_single(cls, **data):
+        numerator = DerivedKind.s1
+        denominator = DerivedKind.s2
+
+        numer = []
+        denomin = []
+
+        for base_kind in data[numerator]:
+            numer.append(BaseKind(
+                base_kind[0],
+                base_kind[1]
+            ))
+        for base_kind in data[denominator]:
+            denomin.append(BaseKind(
+                base_kind[0],
+                base_kind[1]
+            ))
+        d = {'numerator': numer,
+             'denominator': denomin}
+        return cls(**d)
+
+    def __init__(self, **units: Kind):
+        super().__init__()
+        numerator = DerivedKind.s1
+        denominator = DerivedKind.s2
+        self.__numerator = units[numerator]  # list if Kinds
+        self.__denominator = units[denominator]  # list if Kinds
+        self.__multiplier = self.__generate_multiplier()
+
+    def __generate_multiplier(self):
+        multiplier = 1
+        for kind in self.__numerator:
+            multiplier *= kind.get_multiplier()
+        for kind in self.__denominator:
+            multiplier *= kind.get_multiplier()
+        return multiplier
+
+    def get_unit_string(self):
+        s1 = '*'.join(i.get_unit_string for i in self.__numerator)
+        s2 = '*'.join(i.get_unit_string for i in self.__denominator)
+        return f'({s1})/({s2})'
+
+    def set_prefix(self):
+        pass
+
+    def to_si(self):
+        pass
 
 
 class MeasUnit:
@@ -58,52 +187,52 @@ class MeasUnit:
         self.__category = category
         self.__kind = kind
 
+    def __str__(self):
+        return self.get_unit_string()
+
+    def __repr__(self):
+        return str(self)
+
     def get_category(self):
         return self.__category
 
     def get_multiplier(self):
         return self.__kind.get_multiplier()
 
-    def get_unit(self):
-        return self.__kind.get_unit()
-
-    def __str__(self):
-        return self.get_unit()
-
-    def __repr__(self):
-        return str(self)
+    def get_unit_string(self):
+        return self.__kind.get_unit_string()
 
 
-class Parser:
-    __Base_units = []
-    with open('units.json', 'r', encoding='utf8') as f:
-        data = json.load(f)
-        for unit in data['unit'].keys():
-            __Base_units.append(data['unit'][unit][1])
-
-    @staticmethod
-    def __reverse_all(strings):
-        for i in range(len(strings)):
-            strings[i] = strings[i][::-1]
-        return strings
-
-    @staticmethod
-    def __end_replace(s: str, s1: str, s2: str):
-        a = [s, s1, s2]
-        a = Parser.__reverse_all(a)
-        s = s.replace(a[1], a[2], 1)
-        s = s[::-1]
-        return s
-
-    # now it is a simplistic parser!!! from unit to (prefix, base_unit)
-    @staticmethod
-    def parse(unit: str):
-        for base_unit in Parser.__Base_units:
-            if unit.endswith(base_unit):
-                if unit == base_unit:
-                    return '', base_unit
-                prefix = Parser.__end_replace(unit, base_unit, '')
-                return prefix, base_unit
+# class Parser:
+#     __Base_units = []
+#     with open('units.json', 'r', encoding='utf8') as f:
+#         data = json.load(f)
+#         for unit in data['unit'].keys():
+#             __Base_units.append(data['unit'][unit][1])
+#
+#     @staticmethod
+#     def __reverse_all(strings):
+#         for i in range(len(strings)):
+#             strings[i] = strings[i][::-1]
+#         return strings
+#
+#     @staticmethod
+#     def __end_replace(s: str, s1: str, s2: str):
+#         a = [s, s1, s2]
+#         a = Parser.__reverse_all(a)
+#         s = s.replace(a[1], a[2], 1)
+#         s = s[::-1]
+#         return s
+#
+#     # now it is a simplistic parser!!! from unit to (prefix, base_unit)
+#     @staticmethod
+#     def parse(unit: str):
+#         for base_unit in Parser.__Base_units:
+#             if unit.endswith(base_unit):
+#                 if unit == base_unit:
+#                     return '', base_unit
+#                 prefix = Parser.__end_replace(unit, base_unit, '')
+#                 return prefix, base_unit
 
 
 class TextKinds(enum.Enum):
@@ -112,6 +241,7 @@ class TextKinds(enum.Enum):
     list = 'list'
     title = 'title'
     default = 'default'
+
     par_val = 'parameter_value'
     par_abs_err = 'parameter_absolute_error'
     par_rel_err = 'parameter_relative_error'
