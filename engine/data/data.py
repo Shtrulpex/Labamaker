bcc8873cecf201df75f6d0c2dbb50df1c3ca4842
Vsevolod Fedorov <v3531946@hotmail.cpm>
2022-05-30 14:44:34 +0300

    Added test examples to data.py

--- engine/data/data.py
+++ engine/data/data.py
@@ -113,3 +113,52 @@ class DataController:
 
         result_folder = f'..\\..\\results\\{self.lab}'
         self.result: DataResult = DataResult(result_folder)
+
+
+dc = DataController('lab_111')
+d = dc.material.get_parameters()[0]
+D = dc.material.get_parameters()[1]
+N = dc.material.get_parameters()[3]
+print(d)
+print(D)
+print(N)
+dDd = d * D * d
+print(dDd)
+dDd >> 3
+print(dDd)
+unit = dDd.get_unit_numerator()[0]
+dDd.set_prefix(unit, 'm')
+print(dDd)
+dDd.set_prefix(unit, 'M')
+print(dDd)
+dDd << 2
+print(dDd)
+print(d)
+print(D)
+DN = N / D ** 2
+DN >> 5
+print(DN)
+print('All is good')
+
+
+# testing Tables:
+table = dc.source.get_tables()[0]
+
+print(table)
+print(table.keys())
+table['R_ev'] = d
+table['R'][3] = D
+table['R_ev'][3] *= 2
+table['R_ev'] *= table['R'][3]
+print(table)
+print()
+print(table.iloc[2])
+print()
+
+
+table1 = dc.material.get_tables()[0]
+print(table1)
+table1.insert(2, '12321', table['R_ev'])
+print(table1)
+
+print('All is good')
