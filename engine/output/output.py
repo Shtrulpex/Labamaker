65603bcbfc99f7897c16f496c441ef0685733142
Vsevolod Fedorov <v3531946@hotmail.cpm>
2022-05-31 17:16:58 +0300

    Output first success!

--- engine/output/output.py
+++ engine/output/output.py
@@ -1,36 +1,62 @@
 from pylatex import Document, Section, Subsection, Tabular
-from pylatex import Math, TikZ, Axis, Plot, Figure, Matrix, Alignat
-from pylatex.utils import italic
-import os
+from pylatex import Math, Alignat, Itemize, Command
+from pylatex.utils import italic, bold
 
-if __name__ == '__main__':
-    image_filename = os.path.join(os.path.dirname(__file__), 'kitten.jpg')
-
-    geometry_options = {"tmargin": "1cm", "lmargin": "10cm"}
-    doc = Document(geometry_options=geometry_options)
-
-    # creating a pdf with title "the simple stuff"
-    with doc.create(Section('The simple stuff')):
-        doc.append('Some regular text and some')
-        doc.append(italic('italic text. '))
-        doc.append('\nAlso some crazy characters: $&#{}')
-        with doc.create(Subsection('Math that is incorrect')):
-            doc.append(Math(data=['2*3', '=', 9]))
-
-        # creating subsection of a pdf
-        with doc.create(Subsection('Table of something')):
-            with doc.create(Tabular('rc|cl')) as table:
-                table.add_hline()
-                table.add_row((1, 2, 3, 4))
-                table.add_hline(1, 2)
-                table.add_empty_row()
-                table.add_row((4, 5, 6, 7))
-
-    # making a pdf using .generate_pdf
-    doc.generate_pdf('full', clean_tex=True)
+import shutil
 
 
-class Output:
-    def __init__(self, ):
-        pass
+class Template:
+    def __init__(self,
+                 result_path: str,
+                 geometry_options=None
+                 ):
+        if geometry_options is None:
+            geometry_options = {"tmargin": "1.5cm", "lmargin": "2cm", "rmargin": "2cm"}
+        self.__geomytry_options = geometry_options
+        self.__doc = Document(geometry_options=geometry_options)
+        self.result_path = result_path
+        self.number_of_parameters = None
+        self.parameters = None
+        self.__load_template('')
 
+    def fill(self, **kwargs):
+        r = 2
+        pi = 3.14
+        if len(kwargs) != self.number_of_parameters:
+            raise RuntimeError(f'Incorrect number of parameters to write in PDF')
+        doc = self.__doc
+        with doc.create(Section(bold('Data Processing'))):
+            with doc.create(Itemize()) as itemize:
+                itemize.add_item('Wire resistivity: ')
+                with doc.create(Alignat(numbering=False, escape=False)) as agn:
+                    agn.append(r'{\rho}=\frac{RS}{l}')
+            with doc.create(Subsection(bold('Wire section area'))):
+                with doc.create(Alignat(numbering=False, escape=False)) as agn:
+                    agn.append(r'S=\pi r^2=')
+                    s = f'{r}^2'
+                    agn.append(fr'\frac{pi}{s}')
+
+            with doc.create(Subsection(bold('Turn length'))):
+                pass
+
+            with doc.create(Subsection(bold('LSM'))):
+                with doc.create(Alignat(numbering=False, escape=False)) as agn:
+                    agn.append(r'{R_n}=\frac{\rho l}{S} n=k x + b\\')
+                    agn.append(italic(r'b=0'))
+
+    def generate_pdf(self, filename):
+        self.__doc.generate_pdf(filename, clean_tex=True)
+        # shutil.move(__file__ + '\\' + filename, self.result_path)
+
+    # load template from file,
+    # set Template attributes:
+    #   - number_of_parameters
+    #   - self.parameters
+    def __load_template(self, file: str):
+        self.number_of_parameters = 0
+        self.parameters = {}
+
+
+tmp = Template(__file__)
+tmp.fill()
+tmp.generate_pdf('new_full')
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
 from engine.engine import *
 
 Laba111(DataController('lab_111')).make_laba()
-
